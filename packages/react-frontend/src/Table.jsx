718069c5894243ca5de6736f799a730b806e15e3
kaitlynbresnan <kaitlynbresnan@gmail.com>
2026-01-09 16:06:59 -0800

    Update Table.jsx

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -16,9 +16,14 @@ function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
-        <td>{row.name}</td>
-        <td>{row.job}</td>
-      </tr>
+      <td>{row.name}</td>
+      <td>{row.job}</td>
+      <td>
+        <button onClick={() => props.removeCharacter(index)}>
+          Delete
+        </button>
+      </td>
+    </tr>
     );
    }
   );
@@ -33,7 +38,10 @@ function Table(props) {
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={props.characterData} />
+        <TableBody
+        characterData={props.characterData}
+        removeCharacter={props.removeCharacter}
+      />
       </table>
     );
 }
