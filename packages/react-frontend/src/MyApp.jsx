69746dd9603c89fcf8275492a3f0d965ec1c896c
kaitlynbresnan <kaitlynbresnan@gmail.com>
2026-01-12 08:47:28 -0800

    Update MyApp.jsx

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,6 +1,7 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
